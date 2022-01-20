2cb2ec92353a63a3a55341a217a8de9b254772bc
R304U07 <R304U07@R304PC07.ets.local>
2022-01-20 15:59:56 +0100

    edit engel.java

--- Q1-2-Info/src/engel.java
+++ Q1-2-Info/src/engel.java
@@ -1,4 +1,4 @@
 
 public class engel {
-	//Hallo, mein name ist Niklas Engel und ich habe sehr lange auf diesen Moment gewartet ._.
+	//Hallo, mein name ist Niklas Engel und ich habe sehr lange auf diesen Moment gewartet ._. Test
 }
